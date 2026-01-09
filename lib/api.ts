3e11776811aa4442f04e908a07ca90f35c1545a1
Ajit Borgude <ajit.borgude@pdrl.in>
2026-01-09 18:10:06 +0530

    Added zustand and fetch products api

---
+++ lib/api.ts
@@ -0,0 +1,15 @@
+import { Product } from "@/types/product";
+
+export async function fetchProducts(): Promise<Product[]> {
+  const res = await fetch("https://fakestoreapi.com/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return res.json();
+}
+
+
